b145a26ebbe074d7afe1464aaee3e5a3058dcf88
devesh tanwar <deveshtanwar07@gmail.com>
2023-08-19 00:46:01 +0530

    2nd commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,23 +6,10 @@ const Navbar = (props) =>{
       <nav className={`navbar navbar-expand-sm bg-${props.mode} navbar-${props.mode}`}>
         <div className="container-fluid">
           <a href="/" className="navbar-brand">{props.title}</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a href="/" className="nav-link">About</a>
-              </li>
-              <li className="nav-item">
-                <a href="/" className="nav-link">Home</a>
-              </li>
-            </ul>
             <div className="form-check form-switch">
               <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick = {props.toggleMode}/>
               <label className={`form-check-label text-${props.mode === 'dark' ? 'light' : 'dark'}`}htmlFor="flexSwitchCheckDefault">Enable dark Mode</label>
             </div>
-          </div>
         </div>
       </nav>
     );
